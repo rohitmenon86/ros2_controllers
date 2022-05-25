e78849fc096d523289225f3516fe375637cd54fc
rohit.menon <rohit.menon@uni-bonn.de>
2022-05-25 12:13:30 +0200

    Publish arm and gripper goals in single node. After publishing once, destroy the node

--- ros2_controllers_test_nodes/ros2_controllers_test_nodes/publisher_joint_trajectory_controller.py
+++ ros2_controllers_test_nodes/ros2_controllers_test_nodes/publisher_joint_trajectory_controller.py
@@ -27,83 +27,124 @@ class PublisherJointTrajectory(Node):
     def __init__(self):
         super().__init__("publisher_position_trajectory_controller")
         # Declare all parameters
-        self.declare_parameter("controller_name", "joint_trajectory_controller")
-        self.declare_parameter("wait_sec_between_publish", 6)
-        self.declare_parameter("goal_names", ["pos1", "pos2"])
-        self.declare_parameter("joints", ["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"])
+        self.declare_parameter("controller_name_arm", "joint_trajectory_controller")
+        self.declare_parameter("controller_name_gripper", "rg2_gripper_trajectory_controller")
+        self.declare_parameter("wait_sec_between_publish", 10)
+        self.declare_parameter("goal_names_arm", ["start_pos_arm"])
+        self.declare_parameter("goal_names_gripper", ["start_pos_gripper"])
+        self.declare_parameter("joints_arm", ["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"])
+        self.declare_parameter("joints_gripper", ["rg2_finger_joint1", "rg2_finger_joint2"])
         self.declare_parameter("check_starting_point", False)
         self.declare_parameter("starting_point_limits")
 
         # Read parameters
-        controller_name = self.get_parameter("controller_name").value
+        controller_name_arm = self.get_parameter("controller_name_arm").value
+        controller_name_gripper = self.get_parameter("controller_name_gripper").value
         wait_sec_between_publish = self.get_parameter("wait_sec_between_publish").value
-        goal_names = ["pos1"] #self.get_parameter("goal_names").value
-        self.joints = ["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"] #self.get_parameter("joints").value
+        goal_names_arm = ["start_pos_arm"] #self.get_parameter("goal_names").value
+        goal_names_gripper = ["start_pos_gripper"] #self.get_parameter("goal_names").value
+        self.joints_arm = ["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"] #self.get_parameter("joints").value
+        self.joints_gripper =  ["rg2_finger_joint1", "rg2_finger_joint2"]
         self.check_starting_point = self.get_parameter("check_starting_point").value
         self.starting_point = {}
 
-        if self.joints is None or len(self.joints) == 0:
-            raise Exception('"joints" parameter is not set!')
+        # if self.joints is None or len(self.joints) == 0:
+        #     raise Exception('"joints" parameter is not set!')
 
-        # starting point stuff
-        if self.check_starting_point:
-            # declare nested params
-            for name in self.joints:
-                param_name_tmp = "starting_point_limits" + "." + name
-                self.declare_parameter(param_name_tmp, [-2 * 3.14159, 2 * 3.14159])
-                self.starting_point[name] = self.get_parameter(param_name_tmp).value
-
-            for name in self.joints:
-                if len(self.starting_point[name]) != 2:
-                    raise Exception('"starting_point" parameter is not set correctly!')
-            self.joint_state_sub = self.create_subscription(
-                JointState, "joint_states", self.joint_state_callback, 10
-            )
+        self.joint_state_sub = self.create_subscription(
+            JointState, "/joint_states", self.joint_state_callback, 10
+        )
         # initialize starting point status
+        self.joint_state_sub
         self.starting_point_ok = not self.check_starting_point
 
         self.joint_state_msg_received = False
 
         # Read all positions from parameters
-        self.goals = []
-        for name in goal_names:
+        self.goals_arm = []
+        for name in goal_names_arm:
             self.declare_parameter(name)
-            goal = [1.0, -1.57, 0.0, 0.0, 1.0, 0.2]#self.get_parameter(name).value
-            if goal is None or len(goal) == 0:
+            goal_arm = [0, -1.57, 0.0, 0.0, 1.57, 0.0]#self.get_parameter(name).value
+            if goal_arm is None or len(goal_arm) == 0:
                 raise Exception(f'Values for goal "{name}" not set!')
 
-            float_goal = []
-            for value in goal:
-                float_goal.append(float(value))
-            self.goals.append(float_goal)
+            float_goal_arm = []
+            for value in goal_arm:
+                float_goal_arm.append(float(value))
+            self.goals_arm.append(float_goal_arm)
 
-        publish_topic = "/" + controller_name + "/" + "joint_trajectory"
+        publish_topic_arm = "/" + controller_name_arm + "/" + "joint_trajectory"
 
         self.get_logger().info(
             'Publishing {} goals on topic "{}" every {} s'.format(
-                len(goal_names), publish_topic, wait_sec_between_publish
+                len(goal_names_arm), publish_topic_arm, wait_sec_between_publish
             )
         )
 
-        self.publisher_ = self.create_publisher(JointTrajectory, publish_topic, 1)
+        self.goals_gripper = []
+        for name in goal_names_gripper:
+            self.declare_parameter(name)
+            goal_gripper = [0.0, 0.0]#self.get_parameter(name).value
+            if goal_gripper is None or len(goal_gripper) == 0:
+                raise Exception(f'Values for goal "{name}" not set!')
+
+            float_goal_gripper = []
+            for value in goal_gripper:
+                float_goal_gripper.append(float(value))
+            self.goals_gripper.append(float_goal_gripper)
+
+        publish_topic_gripper = "/" + controller_name_gripper + "/" + "joint_trajectory"
+        self.get_logger().info(
+            'Publishing {} goals on topic "{}" every {} s'.format(
+                len(goal_names_gripper), publish_topic_gripper, wait_sec_between_publish
+            )
+        )
+
+        self.publisher_arm = self.create_publisher(JointTrajectory, publish_topic_arm, 1)
+        self.publisher_gripper = self.create_publisher(JointTrajectory, publish_topic_gripper, 1)
         self.timer = self.create_timer(wait_sec_between_publish, self.timer_callback)
         self.i = 0
+        self.goal_published = False
+
+        self.goal_published_arm = False
+        self.goal_published_gripper = False
 
     def timer_callback(self):
+        if not self.joint_state_msg_received:
+            self.get_logger().warn('No joint state received')
+            return
+        
+        self.goal_published = self.goal_published_arm and self.goal_published_gripper
 
-        if self.starting_point_ok:
+        if self.goal_published:
+            self.get_logger().warn('Goal published. Destroy node')
+            self.destroy_node()
+            raise Exception('Throwing exception to destroy node completely')
 
-            traj = JointTrajectory()
-            traj.joint_names = self.joints
+        if not self.goal_published_arm:
+            self.get_logger().warn('Publishing arm goal...')
+            traj_arm = JointTrajectory()
+            traj_arm.joint_names = self.joints_arm
             point = JointTrajectoryPoint()
-            point.positions = self.goals[self.i]
-            point.time_from_start = Duration(sec=4)
+            point.positions = self.goals_arm[self.i]
+            point.time_from_start = Duration(sec=2)
 
-            traj.points.append(point)
-            self.publisher_.publish(traj)
+            traj_arm.points.append(point)
+            self.publisher_arm.publish(traj_arm)
+            self.goal_published_arm = True
 
-            self.i += 1
-            self.i %= len(self.goals)
+        if not self.goal_published_gripper:
+            self.get_logger().warn('Publishing gripper goal...')
+            traj_gripper = JointTrajectory()
+            traj_gripper.joint_names = self.joints_gripper
+            point = JointTrajectoryPoint()
+            point.positions = self.goals_gripper[self.i]
+            point.time_from_start = Duration(sec=2)
+
+            traj_gripper.points.append(point)
+            self.publisher_gripper.publish(traj_gripper)
+            self.goal_published_gripper = True
+            
 
         elif self.check_starting_point and not self.joint_state_msg_received:
             self.get_logger().warn(
@@ -113,36 +154,39 @@ class PublisherJointTrajectory(Node):
             self.get_logger().warn("Start configuration is not within configured limits!")
 
     def joint_state_callback(self, msg):
-
         if not self.joint_state_msg_received:
 
             # check start state
-            limit_exceeded = [False] * len(msg.name)
-            for idx, enum in enumerate(msg.name):
-                if (msg.position[idx] < self.starting_point[enum][0]) or (
-                    msg.position[idx] > self.starting_point[enum][1]
-                ):
-                    self.get_logger().warn(f"Starting point limits exceeded for joint {enum} !")
-                    limit_exceeded[idx] = True
+            if self.check_starting_point:
+                limit_exceeded = [False] * len(msg.name)
+                for idx, enum in enumerate(msg.name):
+                    if (msg.position[idx] < self.starting_point[enum][0]) or (
+                        msg.position[idx] > self.starting_point[enum][1]
+                    ):
+                        self.get_logger().warn(f"Starting point limits exceeded for joint {enum} !")
+                        limit_exceeded[idx] = True
 
-            if any(limit_exceeded):
-                self.starting_point_ok = False
-            else:
-                self.starting_point_ok = True
+                if any(limit_exceeded):
+                    self.starting_point_ok = False
+                else:
+                    self.starting_point_ok = True
 
             self.joint_state_msg_received = True
-        else:
-            return
+            self.get_logger().warn('joint state received...')
+
 
 
 def main(args=None):
     rclpy.init(args=args)
-
-    publisher_joint_trajectory = PublisherJointTrajectory()
-
-    rclpy.spin(publisher_joint_trajectory)
-    publisher_joint_trajectory.destroy_node()
-    rclpy.shutdown()
+    try:
+        publisher_joint_trajectory = PublisherJointTrajectory()
+        rclpy.spin(publisher_joint_trajectory)
+    except:
+        print("Entered except")
+        publisher_joint_trajectory.destroy_node()
+    finally:
+        print("Entered finally")
+        rclpy.shutdown()
 
 
 if __name__ == "__main__":
